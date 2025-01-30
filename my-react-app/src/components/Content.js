4a70c033ac83ecceb833b5dc1d9f3ad4acec671c
Steph Burbank <stephburbank@MacBook-Pro.fios-router.home>
2025-01-30 17:39:43 -0500

    Add React app with story components

---
+++ my-react-app/src/components/Content.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Content() {
+    return (
+        <div>
+            <p>Once upon a time, there was beautiful young girl named Rapunzel. She was kept locked in a tall tower by an evil witch. The witch kept her there to prevent her from using her magic long hair, which had the power to heal. Rapunzel lived alone, besides the witch who only visit her once a day to bring food.</p>
+            <p>One day, a prince heard Rapunzel singing from the tower and fell in love with the sound of her voice. He foud a way to climb up the tower, using her hair, and visited regularly. Rapunzel and the prince planed to escape together, but the witch eventually discovered their plan. She was very angry and banished Rapunzel to a distand land. The prince, heartbroken, went on a qquest to find her. After many challeneges, they were reunited and lived happily ever after.  </p>
+        </div>
+    );
+}
+
+export default Content;
---
+++ my-react-app/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer>
+            <p>Moral of the Story: "Courage and hope can lead to freedom."</p>
+        </footer>
+    );
+}
+
+export default Footer;
